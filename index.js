ad35c3654b02a6083bbad872dc697d7ef0cc62cb
Shane Dorsey <scdorsey22@gmail.com>
2022-05-18 11:00:04 -0700

    commit tree a7af03ccf436128d6da221dac225882a2e70c051

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+      customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    const bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+function overwriteBestCustomer() {
+    
+}
